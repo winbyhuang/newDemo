b6a22a5335f0919f8e4d49227065c254b65134ee
winby <361831543@qq.com>
2020-05-10 23:48:28 +0800

    init

---
+++ web/src/main/java/com/winby/demo/WebApplication.java
@@ -0,0 +1,17 @@
+package com.winby.demo;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+
+@Log
+@SpringBootApplication
+public class WebApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(WebApplication.class, args);
+        log.info("启动成功------------------");
+    }
+}
